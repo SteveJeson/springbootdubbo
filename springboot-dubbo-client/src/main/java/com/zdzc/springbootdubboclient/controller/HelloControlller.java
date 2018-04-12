9022a4b6360b0b6c391f4ce27b1fa04811efa14c
liuwei <liuwei@zenchn.cn>
2018-04-12 13:14:48 +0800

    test http param

--- springboot-dubbo-client/src/main/java/com/zdzc/springbootdubboclient/controller/HelloControlller.java
+++ springboot-dubbo-client/src/main/java/com/zdzc/springbootdubboclient/controller/HelloControlller.java
@@ -2,8 +2,8 @@ package com.zdzc.springbootdubboclient.controller;
 
 import com.alibaba.dubbo.config.annotation.Reference;
 import com.zdzc.springbootdubboapi.service.DemoService;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
 
 @RestController
 public class HelloControlller {
@@ -11,13 +11,14 @@ public class HelloControlller {
     @Reference(version="1.0.0")
     private DemoService demoService;
 
-    @RequestMapping("/test")
-    public String test() {
-        return demoService.sayHello("dubbo");
+    @RequestMapping(value = "/test",method = RequestMethod.GET)
+    public String test(@RequestParam(value = "name") String name) {
+        return demoService.sayHello(name);
     }
 
     @RequestMapping("/hello")
     public String hello(){
         return "Welcome to springboot dubbo client!";
     }
+
 }
