d6805d4df0e50ee563de7e53ffc76644b2dc8aeb
liuwei <liuwei@zenchn.cn>
2018-04-16 17:55:15 +0800

    add mybatis-generator

---
+++ springboot-dubbo-api/src/main/java/com/zdzc/base/service/UserService.java
@@ -0,0 +1,7 @@
+package com.zdzc.base.service;
+
+
+public interface UserService {
+
+
+}
---
+++ springboot-dubbo-server/src/main/java/com/zdzc/base/mapper/UserMapper.java
@@ -0,0 +1,17 @@
+package com.zdzc.base.mapper;
+
+import com.zdzc.base.model.User;
+
+public interface UserMapper {
+    int deleteByPrimaryKey(Integer userId);
+
+    int insert(User record);
+
+    int insertSelective(User record);
+
+    User selectByPrimaryKey(Integer userId);
+
+    int updateByPrimaryKeySelective(User record);
+
+    int updateByPrimaryKey(User record);
+}
